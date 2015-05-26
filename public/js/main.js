1da493b19c11ea9f751975abbe9e070d5e13e80f
Bryan Duplantis <duplantis@gmail.com>
2015-05-26 17:18:58 -0500

    updated Gruntfile.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,1 @@
-"use strict";
-
-var hello = "World!?";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9qcy9tYWluLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsSUFBSSxLQUFLLFlBQVksQ0FBQSIsImZpbGUiOiJzcmMvanMvbWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbInZhciBoZWxsbyA9IGBXb3JsZCE/YFxuIl19
+"use strict";var hello="World!?";
